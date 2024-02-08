3408311a434650c13cc253cbeadabf7d764221b1
Owais Ahmad <owaisahmad014@gmail.com>
2024-02-08 22:41:07 +0500

    multiple variable

---
+++ multiple_variable_linear_regression.py
@@ -0,0 +1,53 @@
+import copy, math
+import numpy as np
+import matplotlib.pyplot as plt
+#plt.style.use('./deeplearning.mplstyle')
+np.set_printoptions(precision=2)  # reduced display precision on numpy arrays
+
+
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y_train = np.array([460, 232, 178])
+
+
+# data is stored in numpy array/matrix
+print(f"X Shape: {X_train.shape}, X Type:{type(X_train)})")
+print(X_train)
+print(f"y Shape: {y_train.shape}, y Type:{type(y_train)})")
+print(y_train)
+
+
+
+b_init = 785.1811367994083
+w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+print(f"w_init shape: {w_init.shape}, b_init type: {type(b_init)}")
+
+# Single Prediction element by element
+"""
+Our previous prediction multiplied one feature value by one parameter and added a bias parameter. A direct extension of our previous implementation of prediction to multiple features would be to implement
+
+List item
+List item
+(1) above using loop over each element, performing the multiply with its parameter and then adding the bias parameter at the end.
+
+"""
+
+
+def predict_single_loop(x, w, b):
+    """
+    single predict using linear regression
+
+    Args:
+      x (ndarray): Shape (n,) example with multiple features
+      w (ndarray): Shape (n,) model parameters
+      b (scalar):  model parameter
+
+    Returns:
+      p (scalar):  prediction
+    """
+    n = x.shape[0]
+    p = 0
+    for i in range(n):
+        p_i = x[i] * w[i]
+        p = p + p_i
+    p = p + b
+    return p 
